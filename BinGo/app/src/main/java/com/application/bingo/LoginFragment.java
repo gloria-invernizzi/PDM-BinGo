935d3ac9f353f8a270ef81149aaebf3ab0f96407
saraaMagg <smaggioni04@gmail.com>
2025-11-20 21:47:03 +0100

    prova temi e fragment

--- BinGo/app/src/main/java/com/application/bingo/LoginFragment.java
+++ BinGo/app/src/main/java/com/application/bingo/LoginFragment.java
@@ -2,6 +2,7 @@ package com.application.bingo;
 
 import android.os.Bundle;
 
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -15,13 +16,14 @@ import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link LoginFragment#newInstance} factory method to
+ * Use the  factory method to
  * create an instance of this fragment.
  */
 public class LoginFragment extends Fragment {
 
 
     public LoginFragment() {
+
         // Required empty public constructor
     }
 
